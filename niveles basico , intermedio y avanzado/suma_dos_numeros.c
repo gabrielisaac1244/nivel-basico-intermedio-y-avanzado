8738d1bf27d404390bdbd2b0266707cf524090b9
gabrielisaac1244 <gabrielisaac13190@gmail.com>
2025-04-06 18:56:53 -0400

    Delete niveles basico , intermedio y avanzado directory

--- niveles basico , intermedio y avanzado/suma_dos_numeros.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    int a, b, suma;
-    printf("Ingrese dos números: ");
-    scanf("%d %d", &a, &b);
-    suma = a + b;
-    printf("La suma es: %d\n", suma);
-    return 0;
-}
